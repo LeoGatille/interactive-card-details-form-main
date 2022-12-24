2f23f5d12a6e72a3e315cd93092ee882c4dab772
Leo <leo@initiation.com>
2022-12-24 11:33:15 +0100

    refactor: add typescript

--- interactive-card-details-form-main/dist/index.js
+++ interactive-card-details-form-main/dist/index.js
@@ -1,15 +1,14 @@
-
-
+"use strict";
 const formPlaceholder = {
     cardholderName: 'e.g. Jane Appleseed',
     cardNumber: 'e.g. 1234 5678 9123 0000',
-    expirationDate: {month: 'MM', year: 'YY'},
+    expirationDate: { month: 'MM', year: 'YY' },
     cvc: 'e.g. 123',
-}
-
+};
 const cardPlaceholder = {
     cardNumber: '0000 0000 0000 0000',
     cardholderName: 'Jane Appleseed',
     expirationDate: '00/00',
     cvc: '000'
-}
+};
+//# sourceMappingURL=index.js.map
---
+++ interactive-card-details-form-main/src/index.ts
@@ -0,0 +1,15 @@
+
+
+const formPlaceholder = {
+    cardholderName: 'e.g. Jane Appleseed',
+    cardNumber: 'e.g. 1234 5678 9123 0000',
+    expirationDate: { month: 'MM', year: 'YY' },
+    cvc: 'e.g. 123',
+}
+
+const cardPlaceholder = {
+    cardNumber: '0000 0000 0000 0000',
+    cardholderName: 'Jane Appleseed',
+    expirationDate: '00/00',
+    cvc: '000'
+}
